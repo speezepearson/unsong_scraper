e12deaa143715254e64771be32a2d0b9d5c7d876
Spencer Pearson <suspense@cs.washington.edu>
2017-05-14 21:47:51 -0700

    apply h1/h2 split to interludes/books as well as chapters

--- prettify.sh
+++ prettify.sh
@@ -2,4 +2,4 @@
 
 sed \
     -e 's_\(<h1 id="chap[0-9]*">.*</h1>\)_<center>\1</center>_' \
-    -e 's_\(<h1 id="chap[0-9]*">Chapter [0-9]*\):\(.*\)</h1>_\1</h1><h2>\2</h2>_' \
+    -e 's_\(<h1 id="chap[0-9]*">\(Chapter\|Interlude\|Book\)[^:]*\):\(.*\)</h1>_\1</h1><h2>\3</h2>_' \
